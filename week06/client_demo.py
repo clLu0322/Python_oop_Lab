12c42ef862137a6f375ef574b68b0d5882eb407a
clLu0322 <ching.liang0322@gmail.com>
2024-04-09 21:01:34 +0800

    week06

--- week06/client_demo.py
+++ week06/client_demo.py
@@ -1,31 +1,25 @@
 import socket 
 import json
 
-host = "127.0.0.1"
-port = 20001
 BUFFER_SIZE = 1940
-
 
 class SocketClient:
     def __init__(self, host, port):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         self.client_socket.connect((host, port))
-  
  
     def send_command(self, command, parameters):
-        
-        send_data = {'command': command,'parameters': parameters}
-        print(f"The client sent data => ('command': '{command}'), ('parameters': {parameters})")
+        send_data = {'command': command, 'parameters': parameters}
+        print(f"    The client sent data => ('command': '{command}'), ('parameters': {parameters})")
         self.client_socket.send(json.dumps(send_data).encode())
 
     def wait_response(self):
-
         data = self.client_socket.recv(BUFFER_SIZE)
         raw_data = data.decode()
         return json.loads(raw_data)
 
-
-if __name__ == '__main__':
+"""
+    if __name__ == '__main__':
     client = SocketClient(host, port)
 
     keep_going = True
@@ -33,4 +27,4 @@ if __name__ == '__main__':
         command = input(">>>")
         client.send_command(command)
         keep_going = client.wait_response() 
-    
+"""
